a5191aa5a1f6f4b9312fc20dddc90f7902655dde
Pedro <pedroaraujo4910@gmail.com>
2025-03-14 10:44:22 -0300

    Upgrading FieldToUpdateNotFoundException error message

--- person-core/src/main/java/com/project/person/exception/FieldToUpdateNotFoundException.java
+++ person-core/src/main/java/com/project/person/exception/FieldToUpdateNotFoundException.java
@@ -2,6 +2,6 @@ package com.project.person.exception;
 
 public class FieldToUpdateNotFoundException extends BadRequestException {
     public FieldToUpdateNotFoundException(String field) {
-        super(String.format("Field %s not found", field));
+        super(String.format("Field %s cannot be updated (wasn't found, or can't be changed)", field));
     }
 }
