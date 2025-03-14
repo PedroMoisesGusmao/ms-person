30b0a1359278ee4965373a0a36f9b2f894e4d566
Pedro <pedroaraujo4910@gmail.com>
2025-03-14 09:33:49 -0300

    Adding toString method to Address

--- person-core/src/main/java/com/project/person/domain/Address.java
+++ person-core/src/main/java/com/project/person/domain/Address.java
@@ -3,9 +3,11 @@ package com.project.person.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 public class Address {
 
