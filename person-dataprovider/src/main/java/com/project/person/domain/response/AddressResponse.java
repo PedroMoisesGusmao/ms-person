ce4d068e07bbfce7f7b73d20abe7bd7b1de97ff2
Pedro <pedro.gusmao@picpay.com>
2025-03-28 13:46:10 -0300

    Adding builder to AddressResponse

--- person-dataprovider/src/main/java/com/project/person/domain/response/AddressResponse.java
+++ person-dataprovider/src/main/java/com/project/person/domain/response/AddressResponse.java
@@ -3,12 +3,14 @@ package com.project.person.domain.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@Builder
 @ToString
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
